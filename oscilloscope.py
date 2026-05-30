d2c11540160af0885f49be2e6f5efb56d2e75eea
soundGarden <laursenc11@gmail.com>
2026-05-29 17:11:23 -0700

    Added additional data processing to logging. Now calculates the average of each second and every 30 seconds reports the average of the second averages over the previous 30 seconds. Also reports the percentage difference between the last reported 30 second average and the current 30 second average.

--- oscilloscope.py
+++ oscilloscope.py
@@ -1,5 +1,12 @@
-import matplotlib.pyplot as plt
+import tkinter as tk
 from collections import deque
+
+import matplotlib
+matplotlib.use("TkAgg")
+
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+
 
 class PlantOscilloscope:
     def __init__(self, plant_names, num_plants=8, N=100):
@@ -7,24 +14,31 @@ class PlantOscilloscope:
         self.num_plants = num_plants
         self.plant_names = plant_names
 
-        self.plant_buffers = [deque([0]*self.N, maxlen=self.N) for i in range(self.num_plants)]
-        self.start() # start the plot graph
+        self.plant_buffers = [
+            deque([0] * self.N, maxlen=self.N)
+            for _ in range(self.num_plants)
+        ]
+
+        self.start()
 
     """
     Creates and starts up the plot visualizer
     """
     def start(self):
-        self.plt = plt
-        self.plt.ion()
-        plt.rcParams['toolbar'] = 'None' # get rid of the bottom toolbar
+        plt.style.use("dark_background")
+        plt.rcParams["toolbar"] = "None"
 
-        self.fig, self.ax = self.plt.subplots()
-        self.plt.get_current_fig_manager().full_screen_toggle()
-        # self.fig.subplots_adjust(left=0.15) # some padding for left-hand side
+        self.root = tk.Tk()
+        self.root.title("Sound Garden")
+        self.root.attributes("-fullscreen", True)
+
+        self.root.bind("<Escape>",lambda event: self.root.attributes("-fullscreen", False))
+
+        self.fig, self.ax = plt.subplots(figsize=(14, 8))
+
         self.lines = []
 
         # dark styling to make it look more oscilloscope-like
-        self.plt.style.use('dark_background')
         self.ax.set_facecolor('#000000')
         self.fig.patch.set_facecolor("#1D1D1D")
 
@@ -33,22 +47,41 @@ class PlantOscilloscope:
 
         for i in range(self.num_plants):
             line, = self.ax.plot(
-                self.plant_buffers[i],
+                range(self.N),
+                list(self.plant_buffers[i]),
                 color=colors[i % len(colors)],
                 linestyle=linestyles[i % len(linestyles)],
-                label=self.plant_names[i])
-            self.lines.append(line) 
+                label=self.plant_names[i]
+            )
+
+            self.lines.append(line)
 
         # oscilloscope grid look
         self.ax.set_axisbelow(True)
-        self.ax.grid(True, which='major', linestyle='-', linewidth=0.6, color='#444444')
+
+        self.ax.grid(
+            True,
+            which='major',
+            linestyle='-',
+            linewidth=0.6,
+            color='#444444'
+        )
+
         self.ax.minorticks_on()
-        self.ax.grid(True, which='minor', linestyle=':', linewidth=0.5, color='#333333')
+
+        self.ax.grid(
+            True,
+            which='minor',
+            linestyle=':',
+            linewidth=0.5,
+            color='#333333'
+        )
 
         # Labels and title
-        self.ax.set_title("Bioelectric Signal (Touch Sensor)", color='#AAAAAA')
-        #self.ax.set_ylabel("Charge Time (Voltage Proxy)", color='#AAAAAA')
-        #self.ax.set_xlabel("Time (samples)", color='#AAAAAA')
+        self.ax.set_title(
+            "Bioelectric Signal (Touch Sensor)",
+            color='#AAAAAA'
+        )
 
         # custom legend:
         self.ax.legend(
@@ -63,8 +96,25 @@ class PlantOscilloscope:
         )
 
         self.fig.subplots_adjust(top=0.85)
+
         self.ax.set_ylim(0, 100000)
-        self.ax.tick_params(labelbottom = False, labelleft = False) # get rid of the number labels
+
+        self.ax.tick_params(
+            labelbottom=False,
+            labelleft=False
+        )
+
+        self.canvas = FigureCanvasTkAgg(
+            self.fig,
+            master=self.root
+        )
+
+        self.canvas.draw()
+
+        self.canvas.get_tk_widget().pack(
+            fill=tk.BOTH,
+            expand=True
+        )
 
     """
     Updates all plant lines in the plot
@@ -74,6 +124,15 @@ class PlantOscilloscope:
     def update_plot(self, values):
         for i, val in enumerate(values):
             self.plant_buffers[i].append(val)
+
             self.lines[i].set_ydata(self.plant_buffers[i])
-            self.lines[i].set_xdata(range(len(self.plant_buffers[i])))
-        self.plt.pause(0.01)
+
+            self.lines[i].set_xdata(
+                range(len(self.plant_buffers[i]))
+            )
+
+        self.canvas.draw_idle()
+
+        self.root.update_idletasks()
+        self.root.update()
+
